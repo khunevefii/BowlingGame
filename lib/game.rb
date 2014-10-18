5cadef179f77a2cd94919bd3f37f36dd714c15a2
khunevefii <khunevefii@Jitranuchs-MacBook-Pro.local>
2014-10-18 17:16:16 +0700

    finish writing methods

---
+++ lib/game.rb
@@ -0,0 +1,46 @@
+class Game
+	def initialize
+		@score = 0
+		@rolls = []
+	end
+	
+	def roll(pins)
+		@rolls << pins
+	end
+
+
+	def score()
+		i = 0
+		while(i<18) do
+			if(rolls[i]==10) do
+
+				if(rolls[i+2]==10) do
+					score += 10+10+rolls[i+4]
+					i += 2
+				else do
+					score += 10+rolls[i+2]+rolls[i+3]
+					i += 2
+				end
+			
+			end
+
+			elsif(rolls[i]+rolls[i+1]=10 && rolls[i]!=10) do
+				score += 10+rolls[i+2]
+				i += 2
+			end
+
+			else do
+				score += rolls[i]+rolls[i+1]
+				i += 2
+			end
+		end
+
+		while(i>=18) do
+			if(rolls[i]+rolls[i+1]!=10) do
+				score += rolls[i] + rolls[i+1]
+			else do
+				score += rolls[i] + rolls[i+1] + rolls[i+2]
+			end
+		end		
+	end
+end
---
+++ spec/lib/game_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+require "game"
+
+describe Game do
+	decribe "#roll(pins)" do
+		
+	end
+
+	describe "#score" do
+
+	end
+		
+
+end
